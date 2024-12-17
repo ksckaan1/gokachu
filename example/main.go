8d8b9a251088ac9bcfc984025a6a32f22cca5d93
Kaan Kuscu <me@kaanksc.com>
2024-12-17 21:56:01 +0300

    update examples

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/ksckaan1/gokachu"
@@ -10,8 +11,8 @@ import (
 func main() {
 	cache := gokachu.New[string, string](gokachu.Config{
 		ReplacementStrategy: gokachu.ReplacementStrategyLRU,
-		MaxRecordTreshold:   1000,
-		CleanNum:            100,
+		MaxRecordTreshold:   1_000, // When it reaches 1_000 records,
+		CleanNum:            100,   // Cleans 100 records.
 	})
 	defer cache.Close()
 
@@ -21,16 +22,34 @@ func main() {
 	// Set without TTL
 	cache.Set("get_user_response/user_id:1", "John Doe")
 	cache.Set("get_user_response/user_id:2", "Jane Doe")
-	cache.Set("get_user_response/user_id:3", "Walter White")
-	cache.Set("get_user_response/user_id:4", "Jesse Pinkman")
 
+	// Delete specific key
 	cache.Delete("get_user_response/user_id:1")
 
-	fmt.Println(cache.Get("token/user_id:1"))             // eyJhbGciOiJ..., true
+	// Delete keys with "token" prefix
+	cache.DeleteFunc(func(key, _ string) bool {
+		return strings.HasPrefix(key, "token")
+	})
+
+	// Get (uses comma ok idiom)
+	fmt.Println(cache.Get("get_user_response/user_id:2")) // eyJhbGciOiJ..., true
 	fmt.Println(cache.Get("get_user_response/user_id:1")) // "", false
 
-	fmt.Println("keys", cache.Keys())   // List of keys
+	fmt.Println("keys", cache.Keys()) // List of keys
+
+	// List only keys start with "token"
+	filteredKeys := cache.KeysFunc(func(key, _ string) bool {
+		return strings.HasPrefix(key, "token")
+	})
+	fmt.Println("filteredKeys", filteredKeys)
+
 	fmt.Println("count", cache.Count()) // Number of keys
+
+	// Count only keys start with "token"
+	filteredCount := cache.CountFunc(func(key, _ string) bool {
+		return strings.HasPrefix(key, "token")
+	})
+	fmt.Println("filteredCount", filteredCount)
 
 	cache.Flush() // Deletes all keys
 }
